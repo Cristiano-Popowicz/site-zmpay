ecc2312db48292083aada4c43a80300fa4ee27b5
Cristiano-Popowicz <80283809+Cristiano-Popowicz@users.noreply.github.com>
2021-08-20 10:18:31 -0300

    Cristiano 20-08-2021

--- src/components/Api/buscaEmpresa.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const buscaEmpresa = axios.create({
-    baseURL: 'https://sistema.zmpay.com.br/services/zmchat/buscaEmpresa.php'
-})
-;
-
-export default buscaEmpresa;
